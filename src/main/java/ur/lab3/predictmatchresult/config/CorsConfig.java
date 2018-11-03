d7476be5ed69321cefac82e2054ee5c16c79dad8
Ireneusz Kiełtyka <irek140@poczta.fm>
2018-11-03 20:33:39 +0100

    dodanie komenta2

--- src/main/java/ur/lab3/predictmatchresult/config/CorsConfig.java
+++ src/main/java/ur/lab3/predictmatchresult/config/CorsConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
+//Irek tu był
 @Configuration
 public class CorsConfig {
     public CorsConfig() {
